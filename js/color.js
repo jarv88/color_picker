9542651b7a9e2ffef26e4c70dd3d127ef742b193
jarv88 <joseangel038@gmail.com>
2021-09-30 00:22:15 -0500

    final

--- js/color.js
+++ js/color.js
@@ -1,16 +1,16 @@
 const color = document.querySelector("#color");
 const salida = document.querySelector("#salida");
-const storage = JSON.parse(localStorage.getItem("colorValue")) || "#ffffff";
+//const storage = JSON.parse(localStorage.getItem("colorValue")) || "#ffffff";
 const guardar = document.querySelector("#guardar");
 const colorList = document.querySelector("#colorList");
 const arrayList=JSON.parse(localStorage.getItem("colorValue")) || [];
+const ultima = arrayList[arrayList.length-1];
 //localStorage.setItem("colorValue", "#FFFFFF");
 document.querySelector("#salida").style.color="#FFFFFF";
-document.querySelector("#salida").style.backgroundColor=storage[storage.length-1];
-document.querySelector("#salida").innerHTML=storage[storage.length-1];
-document.querySelector("#color").value=storage[storage.length-1];
-
-
+document.querySelector("#salida").style.backgroundColor=arrayList[arrayList.length-1];
+document.querySelector("#salida").innerHTML=ultima || "#FFFFFF";
+document.querySelector("#color").value=arrayList[arrayList.length-1] || "#FFFFFF";
+document.querySelector("#numeroColores").innerHTML=arrayList.length;
 
 (function(arrayList) {
     for (const color of arrayList) {
@@ -19,7 +19,7 @@ document.querySelector("#color").value=storage[storage.length-1];
         li.appendChild(document.createTextNode(color));
         colorList.appendChild(li);
     }
-})(arrayList)
+})(arrayList);
 
 
 
@@ -39,4 +39,5 @@ guardar.addEventListener("click",()=>{
     colorList.appendChild(li);
     arrayList.push(color.value);
     localStorage.setItem("colorValue", JSON.stringify(arrayList));
+    document.querySelector("#numeroColores").innerHTML=arrayList.length;
 });
