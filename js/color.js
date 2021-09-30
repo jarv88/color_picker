e0ca963fb81ff2e3d83d502111d07d8480d5e136
jarv88 <joseangel038@gmail.com>
2021-09-30 00:10:44 -0500

    creacion de lista desde el local storage

--- js/color.js
+++ js/color.js
@@ -1,13 +1,26 @@
 const color = document.querySelector("#color");
 const salida = document.querySelector("#salida");
-const storage = localStorage.getItem("colorValue") || "#FFFFFF";
+const storage = JSON.parse(localStorage.getItem("colorValue")) || "#ffffff";
 const guardar = document.querySelector("#guardar");
 const colorList = document.querySelector("#colorList");
+const arrayList=JSON.parse(localStorage.getItem("colorValue")) || [];
 //localStorage.setItem("colorValue", "#FFFFFF");
 document.querySelector("#salida").style.color="#FFFFFF";
-document.querySelector("#salida").style.backgroundColor=storage;
-document.querySelector("#salida").innerHTML=storage;
-document.querySelector("#color").value=storage;
+document.querySelector("#salida").style.backgroundColor=storage[storage.length-1];
+document.querySelector("#salida").innerHTML=storage[storage.length-1];
+document.querySelector("#color").value=storage[storage.length-1];
+
+
+
+(function(arrayList) {
+    for (const color of arrayList) {
+        let li = document.createElement("li");
+        li.style.backgroundColor=color;
+        li.appendChild(document.createTextNode(color));
+        colorList.appendChild(li);
+    }
+})(arrayList)
+
 
 
 color.addEventListener("input",()=>{
@@ -17,11 +30,13 @@ color.addEventListener("input",()=>{
     //localStorage.setItem("colorValue", value);
 })
 
-color.addEventListener("change",()=>{ localStorage.setItem("colorValue", color.value)  });
+//color.addEventListener("change",()=>{ localStorage.setItem("colorValue", color.value)  });
 
 guardar.addEventListener("click",()=>{
     let li = document.createElement("li");
     li.style.backgroundColor=color.value;
     li.appendChild(document.createTextNode(color.value));
     colorList.appendChild(li);
+    arrayList.push(color.value);
+    localStorage.setItem("colorValue", JSON.stringify(arrayList));
 });
